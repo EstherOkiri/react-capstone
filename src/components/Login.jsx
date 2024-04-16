6fc3d2af1e3cf6ae68cfbf3a2504cdeb47fc38cd
EstherOkiri <est.okiri@gmail.com>
2024-04-16 21:24:37 +0300

    modify login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,9 +7,9 @@ function Login(){
         <>
             <form action="">
                 <label htmlFor="">username</label>
-                <input type="text" />
+                <input type="text"  placeholder="email"/>
                 <label htmlFor="">Password</label>
-                <input type="submit" />
+                <input type="password" placeholder="password" />
             </form>
             <h4>Don't Have An Account?</h4>
             <button>Sign Up</button>
