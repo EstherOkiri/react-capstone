7e49a949789c5472b5a0bd1fe6fed1c731d8ecbf
EstherOkiri <est.okiri@gmail.com>
2024-04-16 20:38:58 +0300

    added Nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 
 
+
 function Navbar(){
 
     return(
         <>
+            <nav className="flex">
+                <div className="flex-1"> 
+                    <p>Hunt logo</p>
+                </div>
+                <div className="flex-1">
+                    <a href="">Jobs</a>
+                    <a href="">Employers</a>
+                </div>
+            </nav>
+            
         
         </>
     )
