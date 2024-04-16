7ac8d0ea76c5e869eec448d06bef05a4256df2f9
EstherOkiri <est.okiri@gmail.com>
2024-04-16 20:46:16 +0300

    create log in form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,6 +5,14 @@ function Login(){
     return(
 
         <>
+            <form action="">
+                <label htmlFor="">username</label>
+                <input type="text" />
+                <label htmlFor="">Password</label>
+                <input type="submit" />
+            </form>
+            <h4>Don't Have An Account?</h4>
+            <button>Sign Up</button>
         
         </>
     )
