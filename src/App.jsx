950db3d3cc297c4795ef9803ea69162f746237bf
EstherOkiri <est.okiri@gmail.com>
2024-04-16 22:14:53 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Login from './components/Login'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 import Jobs from './components/Jobs'
+import { BrowserRouter as Router, Switch, Route } from  "react-router-dom"
 
 function App() {
   
   return (
     <>
-      <Navbar/>
-      <Login />
-      <Jobs />
+      <Router>
+        <Navbar/>
+          <Switch>
+            <Route exact path='/components/Login'><Login /></Route>
+            <Route exact path='/components/Jobs'><Jobs /></Route>
+        </Switch>
+        <Footer />
+
+      </Router>
       
       
     </>
