1665219910c5b7f9cc16cbd429bd56df5f97565c
EstherOkiri <est.okiri@gmail.com>
2024-04-16 18:14:21 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Footer from './components/footer'
+
+function App() {
+  
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
