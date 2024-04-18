ba17a68cecc422b37c37f7d37292ccfcedd325fe
EstherOkiri <est.okiri@gmail.com>
2024-04-18 20:43:13 +0300

    add authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,42 @@
 import SignUp from "./SignUp"
+import { useState } from "react"
 
 
 function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassrord] = useState('');
+    const [logStatus, setLogStatus] = useState('false');
+    const [error, setError] = useState('');
+
+    const handleSubmit=(event) =>{
+        event.preventDefault();
+
+        // console.log(email);
+        // console.log(password);
+
+        fetch('http://localhost:8000/users')
+            .then(response =>{
+                if (!response.ok){
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then (userData => {
+                const user = userData.find(user => user.email === email);
+                if(!user) {
+                    setError('User not found');
+                    return;
+                }
+                if(user.password !== password){
+                    setError('Incorrect password');
+                    return;
+                }
+                setError('');
+                console.log('User logged in: ', {email});
+
+            })
+
+    }
 
     return(
 
@@ -9,11 +44,23 @@ function Login(){
             <div className="bg-loginbg">
             <div className="p-7" style={{backgroundImage: 'url("/assets/login-bg.jpg")'}}>
                 <h1>Log In</h1>
-                <form action="" className="flex items-center justify-center">
+                <form onSubmit={handleSubmit} action="" className="flex items-center justify-center">
                     <label htmlFor="email" className="font-semibold p-2">Email :</label><br />
-                    <input type="text"  placeholder="email" name="email" id="email"/><br />
+                    <input type="text"  
+                    placeholder="email" 
+                    name="email" 
+                    id="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}/>
+
                     <label htmlFor="password" className="font-semibold p-2">Password</label> 
-                    <input type="password" placeholder="password" name="password" id="password" />
+                    <input 
+                    type="password" 
+                    placeholder="password" 
+                    name="password" 
+                    id="password" 
+                    value={password}
+                    onChange={(e)=>setPassrord(e.target.value)}/>
                     <button type="submit" className="font-semibold p-2">Log In</button>
                 </form>
             </div>
