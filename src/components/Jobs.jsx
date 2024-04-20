f30047b88faa18ee155b0481a5cb3ba07ed28d97
EstherOkiri <est.okiri@gmail.com>
2024-04-20 15:49:03 +0300

    update form and add states

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,16 +1,48 @@
 import JobView from "./JobView"
+import { useState } from "react"
 
 function Jobs(){
+    const [jobTitle, setJobTitle] = useState('');
+    const [location, setLocation] = useState('');
+    const [salary, setSalary] = useState(0);
+    const [ jobType, setJobType] = useState('');
+
 
     return(
 
         <>
             <h1>Filter</h1>
-            <form action="">
-                <label htmlFor="">Title</label>
-                <input type="text" placeholder="Job Title" />
-                <label htmlFor="">Location</label>
-                <input type="text" placeholder="Location" />
+            <form onSubmit = {handleSearch} action="">
+                <label htmlFor="jobTitle">Job Title</label>
+                <input type="text" 
+                placeholder="Job Title" 
+                name="jobTitle"
+                id="jobTitle"
+                value={jobTitle}
+                onChange={(e)=>setJobTitle(e.target.value)}/>
+                <label htmlFor="location">Location</label>
+                <input type="text" 
+                placeholder="location"
+                name="location"
+                id="location"
+                value={location}
+                onChange={(e)=>setLocation(e.target.value)} />
+                <label htmlFor="jobType">Job Type</label>
+                <input type="text" 
+                placeholder="job type"
+                name="jobType"
+                id="jobType"
+                value={jobType}
+                onChange={(e)=>setJobType(e.target.value)} />
+                <label htmlFor="salary">Salary</label>
+                <input type="text" 
+                placeholder="ssalary"
+                name="salary"
+                id="salary"
+                value={salary}
+                onChange={(e)=>setSalary(e.target.value)} />
+                
+
                 <input type="submit" name="searchInput" id="" />
             </form>
             <h2>Latest Jobs</h2>
