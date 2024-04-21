9886a294b251afa3d46f281fcbb13b2aaf97b44e
EstherOkiri <est.okiri@gmail.com>
2024-04-21 23:16:16 +0300

    add routing to search jobs

--- src/components/JobView.jsx
+++ src/components/JobView.jsx
@@ -1,13 +1,38 @@
 import Apply from "./JobApplication"
+import { BrowserRouter as Router, Switch, Route } from  "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react"
+import Jobs from "./Jobs";
+
+
 
 function JobView(){
+
+    const [jobTitle, setJobTitle] = useState('');
+    const [location, setLocation] = useState('');
+    const [company, setCompany] = useState('');
+    const [salary, setSalary] = useState('');
+    const [description, setDescription] = useState('');
+
+    //fetch data
+    fetch('http://localhost:8001/jobs')
+    .then(res =>{
+        if(!res.ok){
+            throw new Error('Network Error')
+        }
+        return res.json();
+    })
+    .then(data =>{
+        console.log(data);
+    })
+    
 
     return(
 
         <>
             <div>
                 <div>
-                    <h1>Job Title</h1>
+                    <h1>Vacancies</h1>
                     <h4>Location</h4>
                     <h4>Salary</h4>
                 </div>
@@ -24,7 +49,15 @@ function JobView(){
                     </ul>
                 </div>
             </div>
-            <button>Apply</button>
+            <button>Apply</button> <br />
+            <Router>
+                <Route exact path = '/Jobs'><Jobs /></Route>
+            </Router>
+            
+
+            <button> <Link to="/Jobs">Search More Jobs</Link></button>
+
+            
         
         </>
     )
