62fcbe67b4ff09ef6ad0ebf05ea80576f4ed8e06
EstherOkiri <est.okiri@gmail.com>
2024-04-21 10:43:52 +0300

    create job application form

--- src/components/JobApplication.jsx
+++ src/components/JobApplication.jsx
@@ -4,6 +4,33 @@ function Apply(){
 
     return(
         <>
+            <h1>Job Application Form</h1>
+            <div>
+                <form action="">
+                    <label htmlFor="Full name">Full Name</label>
+                    <input type="text"
+                    placeholder="Full name"
+                    name="fullName"
+                    id="fullName"
+                    value={fullName}
+                    onChange={(e)=>setFullName(e.target.value.toLocaleLowerCase())}/>
+
+                    <label htmlFor="jobTitle">Job Applied For</label>
+                    <input type="text" 
+                    name="jobTitle" 
+                    id="jobTitle"
+                    value={jobTitle}
+                    onChange={(e) => setJobTitle(e.target.value.toLocaleLowerCase())} />
+
+                    <label htmlFor="resume">Upload Your Resume</label>
+                    <input type="file"
+                    id="resume"
+                    accept=".pdf" />
+
+                    <button type="submit">Apply</button>
+                </form>
+            </div>
+
         
         </>
     )
