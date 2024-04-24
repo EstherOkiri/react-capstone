56fb0d54f78eadbe899103a123d5f048894fe1d8
EstherOkiri <est.okiri@gmail.com>
2024-04-24 22:45:04 +0300

    add logic after application form submit

--- src/components/JobApplication.jsx
+++ src/components/JobApplication.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react"
 import { useLocation } from "react-router-dom"
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function Apply(){
     const [fullName, setFullName] = useState('')
     const [jobTitle, setJobTitle] = useState('')
     const location = useLocation();
+    const [submitted,setSubmitted] = useState(false);
+
 
     useEffect(() => {
         if(location.state && location.state.jobTitle){
@@ -12,18 +15,31 @@ function Apply(){
         }
     },[location.state])
 
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(fullName);
+        console.log (jobTitle);
+        alert("Application submitted successfuly!")
+        setSubmitted(true)
+    }
+
+    if(submitted){
+        return <Redirect to = "JobView" />
+    }
+
     
 
     return(
         <>
             <h1>Job Application Form</h1>
             <div className="flex items-center justify-center">
-                <form action="">
+                <form action="" onSubmit={handleSubmit}>
                     <label htmlFor="Full name" >Full Name</label> <br />
                     <input type="text"
                     placeholder="Full name"
                     name="fullName"
                     id="fullName"
+                    required
                     value={fullName}
                     onChange={(e)=>setFullName(e.target.value.toLocaleLowerCase())}
                     className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:border-customColor"/>
